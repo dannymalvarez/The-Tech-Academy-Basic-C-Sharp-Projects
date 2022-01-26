8a698a176cee168e768f756fba0d1e394d425e56
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-01-26 10:50:58 -0500

    First code upload of C# content.
    This displays a basic message that makes use of escape characters and inserts a new line.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        string doubleQuotes = "Welcome to Acme Accounting Systems \nRemember, we're \"accounting\" on you!";
+        Console.WriteLine(doubleQuotes);
+        Console.ReadLine();
+    }
+}
+
