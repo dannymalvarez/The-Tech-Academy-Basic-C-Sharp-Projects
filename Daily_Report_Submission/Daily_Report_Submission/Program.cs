d7f9d6be7a2304a578325e5971c87cd6515a2d4d
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-01-26 13:40:59 -0500

    Daily Report Submission Assignment
    This was a fun and simple project to display questions and save answers to variables in C#.

---
+++ Daily_Report_Submission/Daily_Report_Submission/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Daily_Report_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string intro = "The Tech Academy.";
+            string intro2 = "Student Daily Report.";
+            Console.WriteLine(intro);
+            Console.WriteLine(intro2);
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string pageNumber = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");
+            string help = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string positiveExp = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+            Console.WriteLine("Thank you for your answers. An instructor will responsd to this shortly. Have a great day!");
+            Console.Read();
+
+
+        }
+    }
+}
