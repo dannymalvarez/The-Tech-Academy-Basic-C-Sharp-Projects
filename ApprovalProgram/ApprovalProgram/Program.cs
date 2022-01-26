b26d533f5fa92f5d39ea8014a8d79850e4e547b0
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-01-26 16:31:35 -0500

    Uploading approval app
    This application checks if the user is approved or not based on various criteria.

---
+++ ApprovalProgram/ApprovalProgram/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace ApprovalProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string ageString = Console.ReadLine();
+            int age = Convert.ToInt32(ageString);
+            Console.WriteLine("Have you ever had a DUI?");
+            string DUIString = Console.ReadLine();
+            bool DUI = Convert.ToBoolean(DUIString);
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speeding = Console.ReadLine();
+            int tickets = Convert.ToInt32(speeding);
+
+            bool Approved = (age > 15 && DUI == false && tickets < 4);
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(Approved);
+            Console.ReadLine();
+
+
+            
+
+
+        }
+    }
+}
