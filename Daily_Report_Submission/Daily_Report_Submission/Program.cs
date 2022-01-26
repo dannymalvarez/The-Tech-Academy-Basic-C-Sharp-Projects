862dd446145da14000f30c034ae603c4663f9735
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-01-26 13:45:13 -0500

    Forgot to include comments.
    Adding them per instructions.

--- Daily_Report_Submission/Daily_Report_Submission/Program.cs
+++ Daily_Report_Submission/Daily_Report_Submission/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 
 namespace Daily_Report_Submission
@@ -7,26 +7,26 @@ namespace Daily_Report_Submission
     {
         static void Main(string[] args)
         {
-            string intro = "The Tech Academy.";
-            string intro2 = "Student Daily Report.";
-            Console.WriteLine(intro);
+            string intro = "The Tech Academy."; // These variables will be automatically displayed
+            string intro2 = "Student Daily Report."; 
+            Console.WriteLine(intro); // this displays the variables
             Console.WriteLine(intro2);
-            Console.WriteLine("What is your name?");
-            string yourName = Console.ReadLine();
-            Console.WriteLine("What course are you on?");
-            string course = Console.ReadLine();
-            Console.WriteLine("What page number?");
-            string pageNumber = Console.ReadLine();
-            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");
-            string help = Console.ReadLine();
-            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
-            string positiveExp = Console.ReadLine();
-            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
-            string feedback = Console.ReadLine();
-            Console.WriteLine("How many hours did you study today?");
-            string hours = Console.ReadLine();
-            Console.WriteLine("Thank you for your answers. An instructor will responsd to this shortly. Have a great day!");
-            Console.Read();
+            Console.WriteLine("What is your name?"); //This asks for users name
+            string yourName = Console.ReadLine(); // This saves user input
+            Console.WriteLine("What course are you on?"); //This asks for user course
+            string course = Console.ReadLine(); // This saves user input
+            Console.WriteLine("What page number?"); // asking for user page number
+            string pageNumber = Console.ReadLine(); // This saves user input
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\""); // asking if user needs help with anything
+            string help = Console.ReadLine(); // This saves user input
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics."); // asking if user would like to share any positive experiences
+            string positiveExp = Console.ReadLine(); // This saves user input
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific."); //asking user if there is any other feedback they would like to share
+            string feedback = Console.ReadLine(); // This saves user input
+            Console.WriteLine("How many hours did you study today?"); //asking how many hours user studied that day
+            string hours = Console.ReadLine(); // This saves user input
+            Console.WriteLine("Thank you for your answers. An instructor will responsd to this shortly. Have a great day!"); //displaying this sentence when program ends. 
+            Console.Read(); // This makes the program keep running even when finished until user exists
 
 
         }
