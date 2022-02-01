0dbb7bd85498190014f2994b309dad35621f7482
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-01 16:16:46 -0500

    Uploading resubmission.
    Incorrectly answered prompt. After Instructor help, code has been corrected and the concept makes more sense.

---
+++ CallingMethods/CallingMethods/MyMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class MyMath //created class
+    {
+        public static int MyAddition(int userInput)
+        {
+            return userInput + 5; // assigning result of user input plus 5 to output for display
+        }
+        public static int MySubtraction(int userInput)
+        {
+            return userInput - 5; // assigning result of user input minus 5 to output for display
+        }
+        public static int MyMultiplication(int userInput)
+        {
+            return userInput * 5; // assigning result of user input times 5 to output for display
+        }
+
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number."); //instructions
+            int userInput = Convert.ToInt32(Console.ReadLine()); //storing user input as an int
+            int addResult = MyMath.MyAddition(userInput); //instatiating method with user input and storing result in variable
+            Console.WriteLine(addResult); //displaying result
+            int subtractResult = MyMath.MySubtraction(userInput); //instatiating method with user input and storing result in variable
+            Console.WriteLine(subtractResult);//displaying result
+            int multiplyResult = MyMath.MyMultiplication(userInput); //instatiating method with user input and storing result in variable
+            Console.WriteLine(multiplyResult);//displaying result
+            Console.ReadLine();
+        }
+    }
+}
