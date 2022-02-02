f32f052a9c186f402d4ec8eca9fcd19aee147912
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-02 11:48:17 -0500

    Method submission
    This app takes in user input that asks if they would like to enter one or two numbers. After which the app either adds the second number they entered to the first, or a default already hard coded in.

---
+++ MethodSubmission/MethodSubmission/MyMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    public class MyMath //created class
+    {
+        public static int MyAddition(int input1, int input2) //creating method with two inputs
+        {
+            return input1 + input2; //return inputs after they are added together
+        }   
+    }
+}
