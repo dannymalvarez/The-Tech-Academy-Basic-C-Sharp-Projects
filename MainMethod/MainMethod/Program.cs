0c1bc891ddf758e3f6da7a603036e7560f6c8181
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-02 14:29:22 -0500

    Uploading main method assignment
    This is the revised version of code for the main method assignment.

---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number."); //instructions
+            int userInputWhole = Convert.ToInt32(Console.ReadLine()); //converting user input to int and storing it
+            int addResult = MyMath.MyAddition(userInputWhole); //instantiating methodand saving it to a variable
+            Console.WriteLine(addResult); //displaying result
+
+
+            Console.WriteLine("Please enter a decimal number."); //instructions
+            decimal userInputDec = Convert.ToDecimal(Console.ReadLine()); //converting user input to decimal and storing it
+            int userDecToInt = Convert.ToInt32(userInputDec); //converting decimal to int
+            decimal divideDecimal = MyMath.MyDecimalDivision(userDecToInt); //instantiating method and saving it to a variable
+            Console.WriteLine(divideDecimal); //displaying result
+
+
+            Console.WriteLine("Please enter a whole number."); //instructions
+            string userInput = Console.ReadLine(); //Storing user input as a string
+            int multiplyResult = MyMath.MyDecimalDivision(userInput);  //instantiating method and saving it to a variable
+            Console.WriteLine(multiplyResult); //displaying result
+            Console.ReadLine();
+        }
+    }
+}
