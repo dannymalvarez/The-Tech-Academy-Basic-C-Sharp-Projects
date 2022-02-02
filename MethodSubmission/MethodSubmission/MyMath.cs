f549cca8c4e49ee41b0394bc30266d401127bb88
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-02 15:11:33 -0500

    Uploading revised assignment

---
+++ MethodSubmission/MethodSubmission/MyMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    public class MyMath //created class
+    {
+        public static void MyAddition(int input1, int input2=15) // creating method with 2 parameters, one of which is optional
+        {
+            int finalOutput = input1 + input2; //storing output/sum of inputs 1 and 2 into variable
+            Console.WriteLine(finalOutput); //printing out variable containing final output
+        }
+
+    }   
+    
+}
