248fdfed3d8d0cb9d073320a1a64879174a1708c
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-02 17:34:15 -0500

    Submitting revision with named parameters

---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath This = new MyMath(); //instantiating class
+            Console.WriteLine("Please enter 2 numbers."); //instructions
+            int input1 = Convert.ToInt32(Console.ReadLine()); //storing user input as int
+            int input2 = Convert.ToInt32(Console.ReadLine());//storing user input as int
+            This.MyAddition(input1: input1, input2: input2);//Calling method and passing in two parameters
+            Console.ReadLine();
+        }
+    }
+}
