1426fd8bb2c568c4ffed580673b12434a79b8d3f
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-03 15:06:19 -0500

    Uploading methods and objects assignment

---
+++ MethodsAndObjects/MethodsAndObjects/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    public class Person //creating class person 
+    {
+        public string FirstName { get; set; } //creating first name property
+        public string LastName { get; set; } //creating last name property
+        public void SayName() //creating void method with no parameters that writes out the first and last names 
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(); //instantiating employee object
+            employee.FirstName = "Sample"; //initializing object with first name of "Sample"
+            employee.LastName = "Student"; //initializing object with last name of "Student"
+            employee.SayName(); //calling super class method SayName() on employee object
+            Console.ReadLine();
+        }
+    }
+}
