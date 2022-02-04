fd28cf6233a0f96c896a78f4f3d5be237b0027b2
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-03 21:08:08 -0500

    Uploading assignment
    Used previous assignment as a base and added new assignment requirements to it.

---
+++ AbstractClassSubmission/AbstractClassSubmission/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmission
+{
+    public class Employee : Person, IQuittable //creating employee class and inheriting from person class and IQuittable interface
+    {
+        public override void SayName() //implemting say name method 
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName); // printing out a full name.
+        }
+        public void Quit()
+        {
+            Console.WriteLine("This is your confirmation that you are quitting."); //displaying this message
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AbstractClassSubmission/AbstractClassSubmission/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(); //instantiating employee object
+            employee.FirstName = "Sample"; //initializing object with first name of "Sample"
+            employee.LastName = "Student"; //initializing object with last name of "Student"
+            employee.SayName(); //calling say name method on employee object
+            IQuittable quit = new Employee(); //creating an object of type IQuittable 
+            quit.Quit(); //calling the Quit() method on new object
+            Console.ReadLine();
+        }
+    }
+}
