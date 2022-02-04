c01e7543f857f9ffc7a81da562866d3833e18510
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-04 13:06:40 -0500

    Uploading for operators submission

---
+++ AbstractClassSubmission/AbstractClassSubmission/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmission
+{
+    public class Employee : Person, IQuittable //creating employee class and inheriting from person class and IQuittable interface
+    {
+        public override void SayName() //implementing say name method 
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName); // printing out a full name.
+        }
+        public void Quit()
+        {
+            Console.WriteLine("This is your confirmation that you are quitting."); //displaying this message
+            Console.ReadLine();
+        }
+        public int Id { get; set; } //Id property of data type int
+        public static bool operator== (Employee employee1, Employee employee2) //overloading == operator which checks if two employee objects are equal by comparing their Id properties
+        {
+            return (employee1.Id == employee2.Id); //return a bool if two employee objects match Ids
+        }
+
+        public static bool operator!= (Employee employee1, Employee employee2) //overloading != operator which checks if two employee objects are not equal by comparing their Id properties
+        {
+            return (employee1.Id != employee2.Id); //return a bool if two employee objects do not match Ids
+        }
+    }
+}
+
---
+++ AbstractClassSubmission/AbstractClassSubmission/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee(); //instantiating employee object
+            employee1.FirstName = "Stephanie"; //initializing object with first name of "Stephanie"
+            employee1.LastName = "Alvarez"; //initializing object with last name of "Alvarez"
+            employee1.Id = 1; //initializing object with id number of 1
+            //employee1.SayName(); //calling say name method on employee object
+
+
+            //IQuittable quit = new Employee(); //creating an object of type IQuittable 
+            //quit.Quit(); //calling the Quit() method on new object
+
+            Employee employee2 = new Employee(); //instantaiting new employee object
+            employee2.FirstName = "Daniel"; //initializing object with first name of "Daniel"
+            employee2.LastName = "Alvarez"; //initializing object with last name of "Alvarez"
+            employee2.Id = 2; //initializing object with id number of 2
+            if (employee1 == employee2) //if employee 1 matches employee 2 print this message below
+            {
+                Console.WriteLine("These two people have the same Id.");
+            }
+            else //if employee 1 does not match employee 2 print this message below
+            {
+                Console.WriteLine("These two people have different Ids.");
+            }
+
+            if (employee1 != employee2) //if employee 1 does not match employee 2 print this message below
+            {
+                Console.WriteLine("These two people have different Ids");
+            }
+            else //if employee 1 matches employee 2 print this message below
+            {
+                Console.WriteLine("These two people have the same Id.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
