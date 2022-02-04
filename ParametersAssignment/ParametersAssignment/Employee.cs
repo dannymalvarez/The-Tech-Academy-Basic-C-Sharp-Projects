b97175e1989852ceb1e7321bc5deca2cb94328fd
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-04 14:19:03 -0500

    Parameters assignment for submission

---
+++ ParametersAssignment/ParametersAssignment/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    public class Employee<T> //setting employee class to take a generic type parameter
+    {
+        public List<T> Things { get; set; } //adding a property with data type to be a generic list matching type of class
+    }
+}
---
+++ ParametersAssignment/ParametersAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employeeString = new Employee<string>(); //instantiating employee object with type string
+            employeeString.Things = new List<string>() { "Apple", "Car", "House", "Computer" }; //assigning list of strings as property value of things 
+
+            Employee<int> employeeInt = new Employee<int>(); //instantiating employee object with type string
+            employeeInt.Things = new List<int>() { 1, 5, 9, }; //assigning list of integers as property value of things 
+
+            foreach (string thing in employeeString.Things) //loop that loops through list of strings and displays them 
+            {
+                Console.WriteLine(thing);
+            }
+            foreach (int thing in employeeInt.Things) //loop that loops through list of integers and displays them 
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+        }
+    }
+}
