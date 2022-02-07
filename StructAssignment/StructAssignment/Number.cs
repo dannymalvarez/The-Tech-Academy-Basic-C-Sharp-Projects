77bec439ae044d85410715616d9257e8734e8f3b
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-07 10:14:13 -0500

    Struct practice
    This practice has me creating a struct and making use of it in the main method. It simply assigns an amount and displays the amount.

---
+++ StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    public struct Number //creating a struct called "Number"
+    {
+        public Decimal Amount { get; set; } //creating a property named "Amount" with type decimal
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number(); //creating an object of data type number
+            num1.Amount = 15.05M; //assigning amount to object num1
+            Console.WriteLine(num1.Amount); //displaying amount 
+            Console.ReadLine();
+        }
+    }
+}
