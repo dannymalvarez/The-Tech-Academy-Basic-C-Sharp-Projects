325091123aa417e759666f07b9135b637e79825d
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-08 14:35:09 -0500

    Uploading Date Time Assignment
    This was an exercise on using DateTime.

---
+++ DateTimeSubmission/DateTimeSubmission/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime TimeNow = DateTime.Now; //storing current time to TimeNow variable
+            Console.WriteLine(TimeNow); //displaying current time to the console
+            Console.WriteLine("Please enter a number between 1 and 24"); //instructions to user 
+            int userInput = Convert.ToInt32(Console.ReadLine()); //storing user input as an int
+            Console.WriteLine("In " + userInput + " hours it will be " + TimeNow.AddHours(userInput) + "."); //displaying the time it will be in however many hours the user input which were added to current time. 
+            Console.ReadLine();
+        }
+    }
+}
