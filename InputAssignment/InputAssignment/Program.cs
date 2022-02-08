6cdfa1117f6954de04d7efcf1d7b1fe903893679
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-08 14:12:16 -0500

    Uploading Input Assignment
    This assignment is an exercise of the write and read all text functionality when using system.IO. Very simple exercise but very helpful in getting a basic understanding of it.

---
+++ InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");//instructions 
+            string input = Console.ReadLine(); //storing user input as a string 
+            File.WriteAllText(@"C:\Users\danny\TTABootCampFiles\Logs\userInput.txt", input); //writing user input to a text file to log it 
+
+            string output = File.ReadAllText(@"C:\Users\danny\TTABootCampFiles\Logs\userInput.txt"); //storing user input from text file to a string variable after reading the file 
+            Console.WriteLine(output); //displaying the output variable to console. 
+            Console.ReadLine();
+
+        }
+    }
+}
