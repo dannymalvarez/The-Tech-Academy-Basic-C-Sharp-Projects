2e5defca365b72929959a54678eb6a0ad768bbcf
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-08 17:19:42 -0500

    Uploading corrected code for resubmission

---
+++ ConstructorSubmission/ConstructorSubmission/ConstructorSubmission/FullName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    public class FullName
+    {
+        public string myFirstName;
+        public string myLastName;
+        
+       
+
+        public FullName(string myFirstName, string myLastName)
+        {
+            this.myFirstName = myFirstName;
+            this.myLastName = myLastName;
+        }
+    }
+}
---
+++ ConstructorSubmission/ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string myFirstName = "Daniel"; //setting a constant string variable as my first name 
+            const string myLastName = "Alvarez"; //creating a variable using var for my last name
+            var fullName = new FullName(myFirstName, myLastName);
+            Console.WriteLine(fullName.myFirstName);
+            Console.WriteLine(fullName.myLastName);
+             
+            Console.ReadLine();
+            
+            
+            
+            
+        }
+    }
+}
