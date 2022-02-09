cd6404d5d9984152924425de1220b411ddb31ab8
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-09 09:18:49 -0500

    instantiating FullName class

--- ConstructorSubmission/ConstructorSubmission/ConstructorSubmission/Program.cs
+++ ConstructorSubmission/ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -11,9 +11,10 @@ namespace ConstructorSubmission
         static void Main(string[] args)
         {
 
+            FullName fullName = new FullName();
             const string myFirstName = "Daniel"; //setting a constant string variable as my first name 
-            const string myLastName = "Alvarez"; //setting a constant string variable as my last  name 
-            var fullName = new FullName(myFirstName, myLastName); //creating a variable using var for passing these constants to the method
+            var myLastName = "Alvarez"; //setting a constant string variable as my last  name 
+            fullName = new FullName(myFirstName, myLastName); //creating a variable using var for passing these constants to the method
             Console.WriteLine(fullName.myFirstName); //displaying first name to console
             Console.WriteLine(fullName.myLastName); //displaying last name to console
              
