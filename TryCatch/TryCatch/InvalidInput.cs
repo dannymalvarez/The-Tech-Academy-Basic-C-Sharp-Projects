3eea71308c3c0c4ca673a3fe943513b0bd4ad76b
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-09 10:17:17 -0500

    Uploading try catch assignment

---
+++ TryCatch/TryCatch/InvalidInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    public class InvalidInput : Exception //created a new class for invalid input exception handling
+    {
+        public InvalidInput()
+            : base() { }
+        public InvalidInput(string message)
+            : base(message) { }
+    }
+}
---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try //try block for this block of code 
+            {
+                Console.WriteLine("Please enter your age."); //instructions
+                int age = Convert.ToInt32(Console.ReadLine()); //storing user input after converting to int
+                if (age <= 0) //checks if age is less than or equal to 0, aka invalid input 
+                {
+                    throw new InvalidInput(); //throw invalid input exception 
+                }
+               
+                int currentYear = 2022; //storing current year as int 
+                int yearBorn = currentYear - age; //storing approximate year born for user based on input subtracted from current yer
+                Console.WriteLine("You were born in " + yearBorn); //displaying a simple sentence with their year born 
+                Console.ReadLine();
+            }
+            catch (InvalidInput) //catch for invalid input error 
+            {
+                Console.WriteLine("Please do not enter 0 or negative numbers.");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception) //catch for any other errors/exceptions 
+            {
+                Console.WriteLine("An error occurred, please contact your System Administrator.");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
