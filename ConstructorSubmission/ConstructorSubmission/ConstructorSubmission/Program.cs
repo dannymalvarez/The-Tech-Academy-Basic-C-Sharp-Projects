faa55afbd9603316cda2a3a4c1275517ae87ff23
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-10 09:31:03 -0500

    revised code

--- ConstructorSubmission/ConstructorSubmission/ConstructorSubmission/Program.cs
+++ ConstructorSubmission/ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -12,10 +12,14 @@ namespace ConstructorSubmission
         {
 
             FullName fullName = new FullName();
-            const string myFirstName = "Daniel"; //setting a constant string variable as my first name 
-            var myLastName = "Alvarez"; //setting a constant string variable as my last  name 
-            fullName = new FullName(myFirstName, myLastName); //creating a variable using var for passing these constants to the method
-            
+            const string firstName = "Stephanie"; //setting a constant string variable as my first name 
+            var lastName = "Hong"; //setting a constant string variable as my last  name 
+            string completeName = fullName.FName + " " + fullName.LName; //creating a variable for displaying fullname with nothing passed in 
+            fullName.FName = firstName; //passing in first name variable
+            fullName.LName = lastName; //passing in last name variable
+            Console.WriteLine(fullName.FName + " " + fullName.LName); //displaying what FName and LName haev been set to/have had passed in
+            Console.WriteLine(completeName); //displaying completename variable which has notyhing passed in. 
+            Console.ReadLine();
         }
     }
 }
