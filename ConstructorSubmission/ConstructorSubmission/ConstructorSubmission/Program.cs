b24d781264cb0b141f4fc92a04d2c7cc5657a73d
Daniel Alvarez <92490029+dannymalvarez@users.noreply.github.com>
2022-02-09 20:27:39 -0500

    Removing unnecessary code

--- ConstructorSubmission/ConstructorSubmission/ConstructorSubmission/Program.cs
+++ ConstructorSubmission/ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -15,13 +15,6 @@ namespace ConstructorSubmission
             const string myFirstName = "Daniel"; //setting a constant string variable as my first name 
             var myLastName = "Alvarez"; //setting a constant string variable as my last  name 
             fullName = new FullName(myFirstName, myLastName); //creating a variable using var for passing these constants to the method
-            Console.WriteLine(fullName.myFirstName); //displaying first name to console
-            Console.WriteLine(fullName.myLastName); //displaying last name to console
-             
-            Console.ReadLine();
-            
-            
-            
             
         }
     }
