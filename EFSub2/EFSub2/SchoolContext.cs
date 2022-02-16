094a3835be04648be6f8e37ceaad56a8d1a396f2
Daniel Alvarez <dannymalvarez@gmail.com>
2022-02-16 14:41:54 -0500

    Building out walk through

---
+++ EFSub2/EFSub2/SchoolContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFSubmission
+{
+    public class SchoolContext: DbContext
+    {
+        public SchoolContext(): base()
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
